65bbef836a0da61d6a4f98a6437cd61a4c30b875
Mamer Mendoza <atmalmer23@gmail.com>
2015-05-04 18:30:12 +0800

    Updated routes. Removed routes that are not needed

--- app/routes.php
+++ app/routes.php
@@ -11,12 +11,11 @@
 |
 */
 
-//Route::get('/test', array('uses' => 'TestController@show', 'as' => 'pages.test'));
+//Controllers for home
 Route::get('/', array('uses' => 'HomeController@showWelcome', 'as' => 'index'));
 Route::post('/', array('uses'=> 'HomeController@showWelcome', 'as' => 'index'));
-//Route::get('/pages',array('uses' => 'GeneralController@show', 'as' => 'page/library'));
 
-//Controller to the web pages in the site
+//Defined by GeneralController to the web pages in the site
 Route::get('pages/about',array('uses' => 'GeneralController@showAbout', 'as' => 'page.about'));
 Route::get('pages/library',array('uses' => 'GeneralController@showLibrary', 'as' => 'page.library'));
 Route::get('pages/curriculum',array('uses' => 'GeneralController@showCurriculum', 'as' => 'page.curriculum'));
@@ -28,11 +27,9 @@ Route::get('pages/profile',array('uses' => 'GeneralController@showProfile', 'as'
 Route::get('pages/publication',array('uses' => 'GeneralController@showPublication', 'as' => 'page.publication'));
 Route::get('pages/register',array('uses' => 'GeneralController@showRegisterForm', 'as' => 'page.reg_form'));
 
-Route::get('/thisIsATestRoute','LaravelHomeControl@showHome');
-
-Route::get('home', function(){
-	return "<h1>This is my home</h1>";
-});
-
+//Resourceful controllers
 Route::resource('pages/students','StudentsController');
 Route::resource('pages/faculty','FacultyController');
+
+//Initial route given by the Laravel project
+Route::get('/thisIsATestRoute','LaravelHomeControl@showHome');
