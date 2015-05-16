4b9de10941ebabef57da7fbf067c58fcd7965b2d
gelloguiam <acguiam@gmail.com>
2015-05-17 02:17:34 +0800

    luffy the slayer gone wild

--- app/views/pages/about.blade.php
+++ app/views/pages/about.blade.php
@@ -3,7 +3,7 @@
 
     {{ HTML::style('css/about.css'); }}
     {{ HTML::script('script/about.js'); }}
-
+ 
     <div>
     	<div class="" id="about-panel1">
     		<div class="wrapping-panel" id="about-panel1-content">
@@ -71,9 +71,80 @@
         <div class="wrapping-panle" id="about-panel4">
             <div id="timeline-content">
                 <div id="tic">
-                    <div id="tic-text">THANK YOU FOR VISITING :)</div>
-                    <img id="tic-icon" src="{{URL::to('res/images/sample2.png')}}">
+                    <div id="tic-icon"></div>
                 </div>
+                <div id="tl-train">
+                    <img src="{{URL::to('res/timeline/train.png')}}">
+                </div>
+
+                <div class="date-entry col-md-12" id="tl-entry-1">
+                    <div class="time-text">The first time Computer Science was
+                        recognized as a discipline in UPLB when the Institute of
+                        Mathematical Sciences and Physics (IMSP) was established with
+                        the Division of Computer Science as on of its divisions.
+                        Dr. Ann Inez N. Gironella was the first director of IMSP and Prof.
+                        Wilfredo Cabezon was the first Division Head of the
+                        Division of Computer Science. IMSP was established when
+                        Dr. Emil Q. Javier was the Chancellor of UPLB. Together with the esablishment of the Division of Computer Science was the institution of the BS Computer Science (BSCS) curriculum in UPLB. Hence, the BS Computer Science Progrm was officially instituted in 1982.
+                    </div>
+                    <div class="tl-photo-wrapper"><img class="tl-photo" src="{{URL::to('res/timeline/IMSP-logo.jpg')}}"></div>
+                </div>
+
+                <div class="date-entry col-md-12" id="tl-entry-2">
+                    <div class="time-text">During the initial implementation of the BSCS program,
+                    three professors and instructors in Statistics handled the computer science courses.
+                    There three are: Dr. Manuel M. Manuel, Prof. Wilfredo E. Cabezon, and
+                    Dr. Eliezer A. Albacea (Instructor then). With the help of the Australian
+                    government through the International Development Program (IDP) of Australian
+                    Univerities and Colleges, the staff of the Division grew in number from initially
+                    three to about ten with graduate degrees in Computer Sience.
+                    The first students in the program were admitted.
+                    </div>
+                    <div class="tl-photo-wrapper"><img class="tl-photo" src="{{URL::to('res/faces/faculty/albacea.jpg')}}"></div>
+                </div>
+
+                <div class="date-entry col-md-12" id="tl-entry-3">
+                    <div class="time-text">
+                    The Division instituted the Master of Science in Computer Science. This became very popular among the teaching staff of the Division. 
+                    </div>
+                    <div class="tl-photo-wrapper"><img class="tl-photo" src="{{URL::to('res/faces/faculty/albacea.jpg')}}"></div>
+                </div>
+
+                <div class="date-entry col-md-12" id="tl-entry-4">
+                    <div class="time-text">
+                    In January 26, 1995, the UP Board of Regents separated the Division of Computer Science from IMSP and merged this with the existing Los Banos Computer Center to form the Institute of Computer Science (ICS). This happened when Dr. Eliezer A. Albacea was the head of the Division, Dr. Ruben Villareal was the Chancellor of UPLB and Dr. Emil Q. Javier was the President of UP. Dr Eliezer A. Albacea became the first Director of ICS.                    </div>
+                    <div class="tl-photo-wrapper"><img class="tl-photo" src="{{URL::to('res/images/ics_logo.jpg')}}"></div>
+                </div>
+
+                <div class="date-entry col-md-12" id="tl-entry-5">
+                    <div class="time-text">
+                        The Institute was designated by Board of Regents as a pilot institute to implement the concept of a System Academi Program. Dr. Eliezer A. Albacea was designated System Director for the UP System Computer Science Program. ICS is instrumental in the institution of BSCS programs in UP Manila, UP in the Visayas (Cebu, Tacloban, and Iloilo), UP College Baguio, and UP Mindanao.
+                        ICS instituted the Diploma in Computer Science both in residential and distance education modes.
+                    </div>
+                    <div class="tl-photo-wrapper"><img class="tl-photo" src="{{URL::to('res/images/ics_logo.jpg')}}"></div>
+                </div>
+
+                <div class="date-entry col-md-12" id="tl-entry-6">
+                    <div class="time-text">
+                        Ph.D. in Computer Science was instituted.
+                    </div>
+                    <div class="tl-photo-wrapper"><img class="tl-photo" src="{{URL::to('res/images/ics_logo.jpg')}}"></div>
+                </div>
+
+                <div class="date-entry col-md-12" id="tl-entry-7">
+                    <div class="time-text">
+                        Master in Information Technology was instituted.
+                    </div>
+                    <div class="tl-photo-wrapper"><img class="tl-photo" src="{{URL::to('res/images/ics_logo.jpg')}}"></div>
+                </div>
+
+                <div class="date-entry col-md-12" id="tl-entry-8">
+                    <div class="time-text">
+                        ICS was designated as National Center of Excellence (COE) in Information Technology by CHED.
+                    </div>
+                    <div class="tl-photo-wrapper"><img class="tl-photo" src="{{URL::to('res/images/ics_logo.jpg')}}"></div>
+                </div>
+
             </div>
         </div>
 
--- public/script/about.js
+++ public/script/about.js
@@ -1,7 +1,85 @@
  $(document).ready(function(){
 
- 	if($("#about-panel4").scrolLeft() == 100) {
- 		alert("Yheye!")
+ 	$("#about-panel4").scrollLeft(0);
+/* 	$("#tl-entry-1 > .time-text").hide(0);
+ 	$("#tl-entry-2 > .time-text").hide(0);
+ 	$("#tl-entry-3 > .time-text").hide(0);
+ 	$("#tl-entry-4 > .time-text").hide(0);
+ 	$("#tl-entry-5 > .time-text").hide(0);
+ 	$("#tl-entry-6 > .time-text").hide(0);
+ 	$("#tl-entry-7 > .time-text").hide(0);
+ 	$("#tl-entry-8 > .time-text").hide(0);
+*/
+
+ 	function timelineSlider(){
+	 	var pos = $("#about-panel4").scrollLeft();
+ 			console.log(pos); 
+		return pos;
+
  	}
+
+/* 	$("#about-panel4").scroll(function(){
+ 		var pos = timelineSlider();
+
+ 		if(pos >= 4100) {
+			$("#tl-entry-8 > .time-text").show("slow");
+ 		}
+
+ 		else if(pos >= 3850) {
+			$("#tl-entry-7 > .time-text").show("slow");
+			$("#tl-entry-8 > .time-text").hide("slow");
+ 		}
+
+ 		else if(pos >= 3440) {
+			$("#tl-entry-6 > .time-text").show("slow");
+			$("#tl-entry-7 > .time-text").hide("slow");
+			$("#tl-entry-8 > .time-text").hide("slow");
+ 		}
+
+ 		else if(pos >= 2640) {
+			$("#tl-entry-5 > .time-text").show("slow");
+			$("#tl-entry-6 > .time-text").hide("slow");
+			$("#tl-entry-7 > .time-text").hide("slow");
+			$("#tl-entry-8 > .time-text").hide("slow");
+ 		}
+
+ 		else if(pos >= 1880) {
+			$("#tl-entry-4 > .time-text").show("slow");
+			$("#tl-entry-5 > .time-text").hide("slow");
+			$("#tl-entry-6 > .time-text").hide("slow");
+			$("#tl-entry-7 > .time-text").hide("slow");
+			$("#tl-entry-8 > .time-text").hide("slow");
+ 		}
+
+ 		else if(pos >= 1400) {
+			$("#tl-entry-3 > .time-text").show("slow");
+			$("#tl-entry-4 > .time-text").hide("slow");
+			$("#tl-entry-5 > .time-text").hide("slow");
+			$("#tl-entry-6 > .time-text").hide("slow");
+			$("#tl-entry-7 > .time-text").hide("slow");
+			$("#tl-entry-8 > .time-text").hide("slow");
+ 		}
+
+ 		else if(pos >= 560) {
+			$("#tl-entry-2 > .time-text").show("slow");
+			$("#tl-entry-3 > .time-text").hide("slow");
+			$("#tl-entry-4 > .time-text").hide("slow");
+			$("#tl-entry-5 > .time-text").hide("slow");
+			$("#tl-entry-6 > .time-text").hide("slow");
+			$("#tl-entry-7 > .time-text").hide("slow");
+			$("#tl-entry-8 > .time-text").hide("slow");
+ 		}
+
+ 		else {
+			$("#tl-entry-2 > .time-text").hide("slow");
+			$("#tl-entry-3 > .time-text").hide("slow");
+			$("#tl-entry-4 > .time-text").hide("slow");
+			$("#tl-entry-5 > .time-text").hide("slow");
+			$("#tl-entry-6 > .time-text").hide("slow");
+			$("#tl-entry-7 > .time-text").hide("slow");
+			$("#tl-entry-8 > .time-text").hide("slow");
+ 		}
+ 	});
+ */
 
  });
