1f3b07d6c1896a002546b5d6c2e72372913bc513
Queen Tin <annekristinemontoya@gmail.com>
2015-05-24 17:58:21 +0800

    paayos nung UI ng profile ni profile.blade.php and ng prfile-edit.blade.php palagyan ng validation both, pagmigrate nung schema

--- app/views/pages/organization.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.default')
-@section('content')
-
-	{{ Form::open(array('files' => true)) }}
-	{{ Form::label('logo', 'Name') }}
-	{{ Form::file('logo') }}
-	{{ Form::submit('Save Changes') }}
-
-@stop
--- app/views/successfile.blade.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-<title>Laravel file Upload : Tutsway.com</title>
-</head>
-<body>
-<p>File Upload Sucessfully.</p>
-</body>
-</html>
